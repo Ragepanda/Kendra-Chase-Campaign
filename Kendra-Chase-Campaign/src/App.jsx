ae8507133eee407d1775ca2bd9141eeb47ee7112
Ragepanda <jwoltz13@gmail.com>
2025-07-31 18:47:46 -0400

    Adding catch-all URL to home page

--- Kendra-Chase-Campaign/src/App.jsx
+++ Kendra-Chase-Campaign/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         {/* <Route path="/about" element={<About />} />
         <Route path="/endorsements" element={<Endorsements />} />
         <Route path="/contact" element={<Contact />} /> */}
